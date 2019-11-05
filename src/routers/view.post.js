fb72dbebd5baf56e942198b20cb9ef6af75b197c
Lawson Cheng <inbox@lawsoncheng.com>
2019-11-06 01:17:43 +0800

    Reading page revamped.

--- src/routers/view.post.js
+++ src/routers/view.post.js
@@ -7,105 +7,102 @@ const request           = require('request');
 const h2p               = require('html2plaintext');
 const requestImageSize  = require('request-image-size');
 const config            = require('../constants/config');
-
-
+const fs                = require('fs');
 
 
 
 /**
  *      --------- Render article page ---------
  */
-router.get('/articles/:articleId', function(req, res) {
-    request({
-        url: config.OS.ENDPOINT + '/articles?ids=' + req.params.articleId,
-        method: 'GET',
-        auth: {
-            'user': config.OS.ID,
-            'pass': config.OS.KEY
+// get the home page
+router.get('/articles/:article_id', (req, res) => {
+    // set data container
+    var article = {};
+    // fetch articles
+    new Promise((resolve, reject) => request({
+        url    : config.OS.ENDPOINT + '/articles?ids=' + req.params.article_id,
+        method : 'GET',
+        auth   : {
+            'user' : config.OS.ID,
+            'pass' : config.OS.KEY
         }
     }, (error, resp, body) => {
-        let result = null, page = null;
+        let result = null;
         try {result = JSON.parse(body)} catch(e) {result = null} finally {result = result || {}}
-        const article = ((result.data || {}).posts || []).shift() || {};
-        if (!article.id) {
-            res.statusCode = 404;
-            res.end('Page not found.');
+        // get article
+        article = ((result.data || {}).rows || []).shift() || {};
+        // next process
+        resolve();
+    }))
+    // get thumbnail data
+    .then(() => new Promise((resolve, reject) => {
+        // get thumbnail
+        const thumb = ((((article || {}).sections || []).shift() || {}).media || []).filter(m => /^(jpe?g|png|svg|git|bmp)$/i.test(m.ext))[0] || null;
+        if (!thumb) {
+            resolve();
         } else {
-            const sections = article.sections || [];
-            new Promise((resolve, reject) => {
-                const thumb = ((sections[0] || {}).medias || []).filter(m => /^(jpe?g|png|svg|git|bmp)$/i.test(m.ext))[0] || null;
-                if (!thumb) {
-                    resolve({});
-                } else {
-                    // download image
-                    requestImageSize(thumb.url)
-                    // failed
-                    .catch(err => resolve({}))
-                    // downloaded
-                    .then(size => {
-                        // next process
-                        resolve({
-                            info: size,
-                            src: thumb.url
-                        });
-                    });
-                }
-            })
-            .then(thumb => {
-                const title = (sections[0] || {}).title || 'Untitled';
-                const desc = h2p((sections[0] || {}).description || '');
-
-                res.set('Content-Type', 'text/html');
-                res.end(Buffer.from('<html>'
-                    + '<head>'
-
-                        + '<meta charset="utf-8"/>'
-                        + `<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>`
-
-                        + `<title>${title}</title>`
-
-                        + `<meta property="og:title" content="${title}"/>`
-                        + `<meta property="og:url" content="https://${req.hostname}${req.originalUrl}"/>`
-                        + (desc.length > 0 ? `<meta property="og:description" content="${desc}"/>` : '')
-                        + (thumb ? [
-                            `<meta name="thumbnail" content="${thumb.src}"/>`,
-                            `<meta property="og:image:width" content="${thumb.info.width}"/>`,
-                            `<meta property="og:image:height" content="${thumb.info.height}"/>`
-                        ].join('') : '')
-
-                    + '</head>'
-                    + '<body style="margin: 0; padding: 0; height: 100%;">' +
-
-
-
-                        sections.map((section, i) => '<section style="' + (i ? 'margin-top: 80px;' : '') + '">' +
-
-                            (i && (section.title || '').length > 0 ? '<h1 style="color: #1c2d5b; margin: 5px 15px 20px;">' + section.title + '</h1>' : '') +
-                            ((section.medias || []).length > 0 ? '<div>' +
-                                '<div class="context">' +
-                                    '<ul style="list-style: none; padding: 0; margin: 0;">' +
-                                        section.medias.map(media => '<li>' +
-                                            '<img src="' + media.url + '" width="100%"/>' +
-                                        '</li>').join('') +
-                                    '</ul>' +
-                                '</div>' +
-                            '</div>' : '') +
-
-                            (!i && (section.title || '').length > 0 ? '<h1 style="color: #1c2d5b; margin: 5px 15px 20px;">' + section.title + '</h1>' : '') +
-                            ((section.description || '').length > 0 ? '<div style="white-space: pre-line; color: #45547d; font-size: 16px; margin: ' + (i ? '10px 15px 0' : '0 15px') + ';">' + h2p(section.description) + '</div>' : '') +
-                        
-                        '</section>').join('')
-
-
-
-                        + '<br/><br/><br/>'
-                    + '</body>'
-                + '</html>'));
-
+            // download image
+            requestImageSize(thumb.url)
+            // failed
+            .catch(err => resolve({}))
+            // downloaded
+            .then(size => {
+                // next process
+                resolve({
+                    info : size,
+                    src  : thumb.url
+                });
             });
         }
-
-    });
+    }))
+    // load html source
+    .then(thumb => fs.readFile(`${__dirname}/../helpers/reader/build/index.html`, (err, data) => {
+        // get html
+        let html = data.toString();
+        // replacements
+        let replacements = {
+            article : JSON.stringify(article) || "{}",
+            title   : (((article || {}).sections || []).shift() || {}).title || 'Error: 404',
+            url     : `http://${req.hostname}/articles/${req.params.article_id}`,
+            desc    : (((article || {}).sections || []).shift() || {}).description || ' ',
+            thumb   : thumb ? [
+                `<meta name="thumbnail" content="${thumb.src}"/>`,
+                `<meta property="og:image:width" content="${thumb.info.width}"/>`,
+                `<meta property="og:image:height" content="${thumb.info.height}"/>`
+            ].join('') : ''
+        };
+        // inject data
+        Object.keys(replacements).forEach(field => html = html.replace(`{{__${field.toUpperCase()}__}}`, replacements[field]));
+        // set header
+        res.setHeader('content-type', 'text/html');
+        // output
+        res.status(200).end(html);
+    }))
+    // any error?
+    .catch(error => res.status(500).json({
+        result  : false,
+        message : error
+    }));
 });
+
+
+// load assets of the file
+router.get('/static/:filePath([a-zA-Z0-9\.\/]+)', (req, res) => fs.readFile(`${__dirname}/../helpers/reader/build/static/${req.params.filePath}`, (err, data) => {
+    let contentType = {
+        js  : 'text/javascript',
+        css : 'text/css',
+        png : 'image/png',
+        jpg : 'image/jpg',
+        svg : 'image/svg+xml',
+        ico : 'image/x-icon',
+        mp4 : 'video/mp4',
+        qt  : 'video/qt'
+    }
+    // set header
+    res.setHeader('content-type', contentType[req.params.filePath.split('.').pop()] || 'text/*');
+    // output
+    res.status(err ? 404 : 200).end(err ? `File not found.` : data.toString());
+}))
+
 
 module.exports = router;
