7e2b20b1147354309b812ce903cd5ccdf07bcf06
Lawson Cheng <inbox@lawsoncheng.com>
2019-11-06 01:18:16 +0800

    Reading page revamped.

---
+++ src/helpers/reader/build/precache-manifest.7eedfa0f18e9f95ae58e3fa85209cad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8180e1f2851306720c7f101ac0c4748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97a19be9bfa723c289e8",
+    "url": "/static/css/main.3a4079ba.chunk.css"
+  },
+  {
+    "revision": "80d7f1a280438f74ea0c",
+    "url": "/static/js/2.88f299f7.chunk.js"
+  },
+  {
+    "revision": "97a19be9bfa723c289e8",
+    "url": "/static/js/main.6a9bf16e.chunk.js"
+  },
+  {
+    "revision": "b66454121b4f5da5ac2d",
+    "url": "/static/js/runtime-main.ca233a98.js"
+  }
+]);
