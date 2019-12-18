4fb465d587db10de6306d7047a59b97c5e073879
harold ho <harold.ho@oneshop.team>
2019-12-18 13:00:39 +0800

    Add a temporary index page and privacy page

--- src/routers/general.js
+++ src/routers/general.js
@@ -3,11 +3,27 @@
  */
 const express = require('express');
 const router  = express.Router();
+const fs                = require('fs');
 
 // set healthcheck route
 router.get('/healthcheck', (req, res) => res.status(200).json({
     result   : true,
     messages : [`no problemo.`]
+}));
+
+// ---------- Home Page ----------
+router.get('/', (req, res) => fs.readFile(`${__dirname}/../assets/index.html`, (err, data) => {
+    // set mime type
+    res.set('content-type', 'text/html');
+    res.status(200).end(data.toString());
+}))
+
+
+// ---------- Privacy policy ----------
+router.get('/privacy_policy', (req, res) => fs.readFile(`${__dirname}/../assets/privacy_policy.html`, (err, data) => {
+    // set mime type
+    res.set('content-type', 'text/html');
+    res.status(200).end(data.toString());
 }));
 
 module.exports = router;
